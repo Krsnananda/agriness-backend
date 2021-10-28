3d691f16cbee9bf3ef6529f676d420d4c3dded0a
Krsnananda <krsnananda@visie.com.br>
2021-10-27 21:51:15 -0300

    Adiciona parâmetros ao token no config files

---
+++ config/default.ts
@@ -0,0 +1,23 @@
+export default {
+  port: 1337,
+  host: "localhost",
+  dbUri: "mongodb+srv://admin:EF7kNsZ9tBFDAEa@agriness-east.uz2md.mongodb.net/agriness?retryWrites=true&w=majority",
+  saltWorkFactor: 10,
+  accessTokenTtl: "15m",
+  refreshTokenTtl: "1y",
+  privateKey: `-----BEGIN RSA PRIVATE KEY-----
+  MIICXAIBAAKBgQCJboEHYluESjaNf7Dhjc6Yw7myyP0OGnLLyYjAw3MCz0x/gspe
+  HS9l48TPf1WJ8ZQuWrxqLDnhl8TQklkrgOZvGQmV3DpXmpmvBluDI4vctiIPUwum
+  GhkgRZgtmtJryshKE71mZIbdysJ7lsnXORgQCHaZdTAkBpOftgdngQxebwIDAQAB
+  AoGAB26KW7Pd7xF0UcKN9iSM/4XWDO0Bgjm763lCg+oN1iEPqfWQq/Y4EfJ3OiFQ
+  pt0BfmA5wCQUHTbv1beUSt31mAfJb+djAKPx6rn8rzznxpzVmD5fwLqFptWp48jh
+  ryYeBFb59y75lC0Bv3tLZv4Ne9aUF+iz9bU6APUQ+NhwPsECQQDhYJkxEUtsMjKQ
+  EMKJbTAvZRzYpBGk8+0McZ3Uyh4r9UUUdI2rCBMYzUwnYSvh1mp6r8tAHTm8H5RP
+  Pbj3FRXFAkEAnBrZ77fIEVNbPbPWclFF6KxmWE9ftNQy+9fdlkBa7QrlblwMqmpW
+  pNEIWmWdXd4ddYK5opddM+/XdaxcYNuaowJBANjlnShiq4JJ4O1So9WcFjZWiedY
+  Jn7oHWbPZQSnK1hEqCGidcB2eeJxmOuBEbk7H6hIzEM8nRaWEaYdEYgAa70CQHZh
+  OnI6mYLiBqg1D+PjVDONQZOopbh4zy0C12xQ42MtRy24qnY1a/1S85Wxx3Pes9Jg
+  TyxIwwpW0LrQ+cEN6CcCQA9vry3OpUg3iz21vONZQzTU8cGbClMs/r09quHqq2v7
+  ePhBqp9Da14H0734NWoHE8pDdkNSSLDXW0PJQxWGPqw=
+  -----END RSA PRIVATE KEY----- `
+}
