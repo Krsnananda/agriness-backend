daf07f975524b644de4d28a3586685b6ad6c35d9
Krsnananda <krsnananda@visie.com.br>
2021-10-28 23:33:46 -0300

    Corrige erro de type

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from "express";
+import express from "express";
 import { omit } from "lodash";
 import { createUser } from "../service/user";
 import log from "../logger";
 
-export async function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(req, res) {
   try {
     const user = await createUser(req.body);
     return res.send(omit(user.toJSON(), "password"));
